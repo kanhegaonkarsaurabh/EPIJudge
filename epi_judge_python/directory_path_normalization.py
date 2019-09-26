ed794e0c075102358305bb0b0eb34b0dde662352
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2019-09-26 14:06:45 -0700

    stack dir and filenames question done

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,10 +1,34 @@
 from test_framework import generic_test
-
+from stack_api import Stack
 
 def shortest_equivalent_path(path):
-    # TODO - you fill in here.
-    return ''
+  stack = Stack()
 
+  # append '/' onto the stack
+  stack.push('/')
+
+  # split the path on the basis of '/'
+  path_names = path.split('/')
+  for name in path_names:
+    if (len(name) != 0 and name != '.'):  # ignore '.' and ''
+      if (name == '..'):                  # pop from the stack
+        if (stack.top() == '/' or stack.top() == '..'): # if there's no dir push .. onto the stack
+          stack.push(name)
+        else:
+          dir_ = stack.pop()
+      else:
+        stack.push(name)
+  new_path = ''
+  val = stack.pop()
+  while (val != '/'):
+    new_path = val + '/' + new_path
+    val = stack.pop()
+
+  # if initial path starts with '/' then push that onto the new_path
+  if (path[0] == '/'):
+    return val + new_path[:-1]
+
+  return new_path[:-1]
 
 if __name__ == '__main__':
     exit(
--- epi_judge_python_solutions/string_integer_interconversion.py
+++ epi_judge_python_solutions/string_integer_interconversion.py
@@ -23,10 +23,10 @@ def int_to_string(x):
 
 
 def string_to_int(s):
-
-    return functools.reduce(
-        lambda running_sum, c: running_sum * 10 + string.digits.index(c),
-        s[s[0] == '-':], 0) * (-1 if s[0] == '-' else 1)
+  return ""
+#    return functools.reduce(
+ #       lambda running_sum, c: running_sum * 10 + string.digits.index(c),
+  #      s[s[0] == '-':], 0) * (-1 if s[0] == '-' else 1)
 
 
 def wrapper(x, s):
