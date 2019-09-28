b21188a4ec6a63b4c994cdf5d445f7bb4d885011
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2019-09-28 00:51:15 -0700

    Add tree level order question solved

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -2,9 +2,25 @@ from test_framework import generic_test
 
 
 def binary_tree_depth_order(tree):
-    # TODO - you fill in here.
-    return []
+  # initialize the deoths of the nodes
+  depths = []
 
+  # queues that store nodes in a depth level
+  q1 = [tree]
+
+  while len(q1) > 0:
+    depths.append([curr.data for curr in q1])
+
+    new_nodes = []
+    for nodes in q1:
+      if (not nodes.left):
+        new_nodes.append(nodes.left)
+      if (not nodes.right):
+        new_nodes.append(nodes.right)
+
+    q1 = new_nodes
+
+  return depths
 
 if __name__ == '__main__':
     exit(
